abd5c187f6eebf48ec9e0f424471be35d2c1fc72
unknown <abhinavvarma46@gmail.com>
2025-11-22 20:30:09 +0530

    DataType

---
+++ src/main/java/org/example/data_types/Non_Primitive_Data_Type.java
@@ -0,0 +1,4 @@
+package org.example.data_types;
+
+public class Non_Primitive_Data_Type {
+}
---
+++ src/main/java/org/example/data_types/Primitive_Data_Type.java
@@ -0,0 +1,37 @@
+package org.example.data_types;
+
+public class Primitive_Data_Type {
+    public static void main(String[] args) {
+        // Integer data type
+        int myInt = 10;
+        System.out.println("Integer value: " + myInt);
+
+        // Floating-point data type
+        float myFloat = 10.5f;
+        System.out.println("Float value: " + myFloat);
+
+        // Character data type
+        char myChar = 'A';
+        System.out.println("Character value: " + myChar);
+
+        // Boolean data type
+        boolean myBool = false;
+        System.out.println("Boolean value: " + myBool);
+
+        // Double data type
+        double myDouble = 20.99;
+        System.out.println("Double value: " + myDouble);
+
+        // Long data type
+        long myLong = 150000L;
+        System.out.println("Long value: " + myLong);
+
+        // Short data type
+        short myShort = 1000;
+        System.out.println("Short value: " + myShort);
+
+        // Byte data type
+        byte myByte = 100;
+        System.out.println("Byte value: " + myByte);
+    }
+}
